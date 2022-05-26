63b92185398e878b4d4b027122914465a8dd8f00
ghdwhd0302 <ghdwhd0302@naver.com>
2022-05-26 16:45:32 +0900

    DAO 추가

---
+++ src/com/member/domain/dao/ReservationDAO.java
@@ -0,0 +1,25 @@
+package com.member.domain.dao;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.member.domain.vo.ReservationVO;
+
+public class ReservationDAO {
+	SqlSessionFactory sqlSessionFactory = MyBatisConfig.getSqlSessionFactory();
+	SqlSession sqlSession;
+	
+	public ReservationDAO() {
+		sqlSession = sqlSessionFactory.openSession(true);
+	}
+	
+	public void reservation(ReservationVO reservationVO) {
+		sqlSession.insert("Member.reservation", reservationVO);
+	}
+	
+	public boolean checkID(String id) {
+		return (Integer)sqlSession.selectOne("Member.checkId", id) == 1;
+	}
+	
+	
+}
