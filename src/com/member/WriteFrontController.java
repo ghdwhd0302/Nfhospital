2d1966e65f17373d5c7d0b950380a996ec3af6e9
yunbo3 <yunbo3@naver.com>
2022-05-30 14:35:53 +0900

    "프론트컨트롤러 수정 및 추가"

--- src/com/member/WriteFrontController.java
+++ src/com/member/WriteFrontController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.member.action.ActionInfo;
 
-public class MemberFrontController extends HttpServlet {
+public class WriteFrontController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -27,31 +27,24 @@ public class MemberFrontController extends HttpServlet {
 		String command = requestURL.substring(requestURL.lastIndexOf("/") + 1);
 		ActionInfo actionInfo = null;
 
-		if (command.equals("JoinOk.me")) {
-			actionInfo = new MemberJoinOk().execute(req, resp);
+		if (command.equals("WriteOk.wr")) {
+			actionInfo = new WriteOk().execute(req, resp);
 
-		} else if (command.equals("Join.me")) {
+		} else if (command.equals("Write.wr")) {
 			actionInfo = new ActionInfo();
 			actionInfo.setRedirect(true);
-			actionInfo.setPath(req.getContextPath() + "/join.jsp");
-		} else if (command.equals("CheckIdOk.me")) {
-			new CheckIdOk().execute(req, resp);
+			actionInfo.setPath(req.getContextPath() + "/write.jsp");
 		} else {
 			// 404 일 때 출력할 에러 페이지 경로 작성
 		}
 
-		
-		
-		
-		
-		if(actionInfo != null) {
-			if(actionInfo.isRedirect()) {
+		if (actionInfo != null) {
+			if (actionInfo.isRedirect()) {
 				resp.sendRedirect(actionInfo.getPath());
-			}else {
+			} else {
 				RequestDispatcher dispatcher = req.getRequestDispatcher(actionInfo.getPath());
 				dispatcher.forward(req, resp);
 			}
 		}
-	
 	}
 }
